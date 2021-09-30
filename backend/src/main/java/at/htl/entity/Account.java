c3758a4fba61e68457596e36a8a4e724dbc4a7f1
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-09-30 17:39:52 +0200

    add annotation

--- backend/src/main/java/at/htl/entity/Account.java
+++ backend/src/main/java/at/htl/entity/Account.java
@@ -1,5 +1,17 @@
 package at.htl.entity;
 
-public class Account {
-    int id;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.*;
+
+@Entity
+@SequenceGenerator(
+        name = "accountSeq"
+        , sequenceName = "V_ACCOUNT_SEQ"
+        , initialValue = 5000
+)
+public class Account extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "accountSeq")
+    Long id;
 }
