8c0c12c25c4b37254e4e2d460d96f5096a7b110a
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-09-30 20:21:20 +0200

    add services

---
+++ backend/src/main/java/at/htl/boundary/CompanyService.java
@@ -0,0 +1,30 @@
+package at.htl.boundary;
+
+import at.htl.control.CompanyRepository;
+import at.htl.entity.Company;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/company")
+@Produces(MediaType.APPLICATION_JSON)
+public class CompanyService {
+    @Inject
+    CompanyRepository repository;
+
+    @GET
+    public List<Company> getAll() {
+        return repository.listAll();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Company post(Company company) {
+        //repository.save(company);
+
+        return company;
+    }
+}
