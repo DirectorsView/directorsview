63dd49af1d2bd9cf84684cc9b9ca7142757cbf8a
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-09-30 20:18:58 +0200

    add fields to Company

--- backend/src/main/java/at/htl/entity/Company.java
+++ backend/src/main/java/at/htl/entity/Company.java
@@ -1,21 +1,41 @@
 package at.htl.entity;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import org.w3c.dom.stylesheets.LinkStyle;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
-public class Company extends Account {
+//@Inheritance(strategy = InheritanceType.JOINED)
+public class Company extends PanacheEntityBase {
     @Column
     private String name;
 
     @JoinColumn
-    @ManyToOne
+    @OneToMany
     private List<Project> projects;
 
     @JoinColumn
-    @ManyToOne
+    @OneToMany
     private List<Gear> inventory;
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Company() {
+    }
+
+    public Company(String name, List<Project> projects, List<Gear> inventory) {
+        this.name = name;
+        this.projects = projects;
+        this.inventory = inventory;
+    }
 }
