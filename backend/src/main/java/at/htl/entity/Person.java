37d1533ac8eaa1782dd0e19394a11fe1bbb52e65
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-05 18:53:10 +0100

    add Table- and Column-names

--- backend/src/main/java/at/htl/entity/Person.java
+++ backend/src/main/java/at/htl/entity/Person.java
@@ -1,15 +1,38 @@
 package at.htl.entity;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
-
 import javax.persistence.*;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "DV_PER")
 public class Person extends Account {
-    @Column
+    @Column(name = "PER_FIRSTNAME")
     String firstName;
 
-    @Column
+    @Column(name = "PER_LASTNAME")
     String lastName;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
