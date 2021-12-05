54f6af7816ec5a80f9e8f94d0dc054b72d5a07ad
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-05 18:18:40 +0100

    define columnnames

--- backend/src/main/java/at/htl/entity/Account.java
+++ backend/src/main/java/at/htl/entity/Account.java
@@ -9,12 +9,16 @@ import javax.persistence.*;
 public class Account extends PanacheEntityBase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ACC_ID")
     private Long id;
 
+    @Column(name = "ACC_PASSWORD")
     private String password;
 
+    @Column(name = "ACC_EMAIL")
     private String email;
 
+    @Column(name = "ACC_WEBSITE")
     private String website;
 
     public Account() {
