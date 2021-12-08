bd8b80b0f1f97dd5fd5e9ebad2458a5e0eef7906
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-08 17:58:31 +0100

    add JAX-RS annotated methods

--- backend/src/main/java/at/htl/boundary/PersonService.java
+++ backend/src/main/java/at/htl/boundary/PersonService.java
@@ -1,10 +1,27 @@
 package at.htl.boundary;
 
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import at.htl.control.PersonRepository;
+import at.htl.entity.Person;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/person")
 @Produces(MediaType.APPLICATION_JSON)
 public class PersonService {
+    @Inject
+    PersonRepository repository;
+
+    @GET
+    public List<Person> getAll() {
+        return repository.listAll();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Person post(Person person) {
+        return repository.save(person);
+    }
 }
