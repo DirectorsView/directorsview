2d8722be6ee556a4376e3be0f838a68a1097bfcb
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-08 17:56:23 +0100

    add annotations

--- backend/src/main/java/at/htl/boundary/PersonService.java
+++ backend/src/main/java/at/htl/boundary/PersonService.java
@@ -1,4 +1,10 @@
 package at.htl.boundary;
 
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/person")
+@Produces(MediaType.APPLICATION_JSON)
 public class PersonService {
 }
