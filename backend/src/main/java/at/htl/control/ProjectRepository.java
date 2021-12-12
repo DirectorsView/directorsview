777af25692a53988d18d561e100616b1d01d1df7
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-12 16:20:40 +0100

    add save method

--- backend/src/main/java/at/htl/control/ProjectRepository.java
+++ backend/src/main/java/at/htl/control/ProjectRepository.java
@@ -1,5 +1,6 @@
 package at.htl.control;
 
+import at.htl.entity.Person;
 import at.htl.entity.Project;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
@@ -9,4 +10,7 @@ import javax.transaction.Transactional;
 @ApplicationScoped
 @Transactional
 public class ProjectRepository implements PanacheRepository<Project> {
+    public Project save(Project project) {
+        return getEntityManager().merge(project);
+    }
 }
