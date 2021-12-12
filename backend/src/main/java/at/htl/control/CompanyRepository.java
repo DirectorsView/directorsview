e5dd9022d9a77250329cff110d97767ac12a6ea3
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-12 11:43:42 +0100

    Inject EntityManager

--- backend/src/main/java/at/htl/control/CompanyRepository.java
+++ backend/src/main/java/at/htl/control/CompanyRepository.java
@@ -4,13 +4,19 @@ import at.htl.entity.Company;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import java.security.PublicKey;
 
 @ApplicationScoped
 @Transactional
 public class CompanyRepository implements PanacheRepository<Company> {
+
+    @Inject
+    EntityManager em;
+
     public Company save(Company company) {
-        return getEntityManager().merge(company);
+        return em.merge(company);
     }
 }
