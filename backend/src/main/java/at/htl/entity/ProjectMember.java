2178a62539eb9b5adf7f54d484474d213e4ceace
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-14 21:51:04 +0100

    implement annotations

--- backend/src/main/java/at/htl/entity/ProjectMember.java
+++ backend/src/main/java/at/htl/entity/ProjectMember.java
@@ -1,10 +1,21 @@
 package at.htl.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "DV_PM")
 public class ProjectMember {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PM_ID")
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "PM_PROJECT")
     private Project project;
 
+    @ManyToOne
+    @JoinColumn(name = "PM_PERSON")
     private Person person;
 }
