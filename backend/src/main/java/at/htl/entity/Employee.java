f17b80dfa3388933f3162c0d9ed5ca794167a97d
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-14 20:17:31 +0100

    add annotations

--- backend/src/main/java/at/htl/entity/Employee.java
+++ backend/src/main/java/at/htl/entity/Employee.java
@@ -2,17 +2,25 @@ package at.htl.entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-
+@Entity
+@Table(name = "DV_EMP")
 public class Employee extends PanacheEntityBase {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "EMP_ID")
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "EMP_PERSON")
     private Person person;
 
+    @ManyToOne
+    @JoinColumn(name = "EMP_COMPANY")
     private Company company;
 
+    @Column(name = "EMP_ISADMIN")
     private Boolean isAdmin;
 }
