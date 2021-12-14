49256f1dfca04786d0f0bd3f1049b0eb4588f607
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-14 20:08:29 +0100

    remove deprecated field

--- backend/src/main/java/at/htl/entity/Company.java
+++ backend/src/main/java/at/htl/entity/Company.java
@@ -17,36 +17,13 @@ public class Company extends Account {
     @Column(name = "COM_ADDRESS")
     private String address;
 
-    /*@JoinColumn(name = "COM_EMPLOYEES")
-    @OneToMany
-    private List<Person> employees;
-
-    @JoinColumn(name = "COM_ADMINS")
-    @OneToMany
-    private List<Person> admins;*/
-
     public Company() {
     }
 
-    public Company(String name, String address) {
-        this.name = name;
-        this.address = address;
-    }
-
-    public Company(String name, String address, List<Person> employees, List<Person> admins) {
-        this.name = name;
-        this.address = address;
-        //this.projects = projects;
-        //this.employees = employees;
-        //this.admins = admins;
-    }
-
-    public Company(String password, String email, String website, String name, String address, List<Person> employees, List<Person> admins) {
+    public Company(String password, String email, String website, String name, String address) {
         super(password, email, website);
         this.name = name;
         this.address = address;
-        //this.employees = employees;
-        //this.admins = admins;
     }
 
     public String getName() {
@@ -64,22 +41,6 @@ public class Company extends Account {
     public void setAddress(String address) {
         this.address = address;
     }
-
-    /*public List<Person> getEmployees() {
-        return employees;
-    }
-
-    public void setEmployees(List<Person> employees) {
-        this.employees = employees;
-    }
-
-    public List<Person> getAdmins() {
-        return admins;
-    }
-
-    public void setAdmins(List<Person> admins) {
-        this.admins = admins;
-    }*/
 
     @Override
     public String toString() {
