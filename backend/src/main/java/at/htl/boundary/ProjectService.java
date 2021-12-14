a4609e130479c89810a7ad5526249d4d562fd930
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-14 21:39:12 +0100

    add getAll Endpoint

--- backend/src/main/java/at/htl/boundary/ProjectService.java
+++ backend/src/main/java/at/htl/boundary/ProjectService.java
@@ -1,4 +1,25 @@
 package at.htl.boundary;
 
+import at.htl.control.ProjectRepository;
+import at.htl.entity.Company;
+import at.htl.entity.Project;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/project")
+@Produces(MediaType.APPLICATION_JSON)
 public class ProjectService {
+
+    @Inject
+    ProjectRepository projectRepository;
+
+    @GET
+    public List<Project> getAll() {
+        return projectRepository.listAll();
+    }
 }
