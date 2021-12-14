5b9af75060692b35ce3fc9ec4bd36045a4bc4f54
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-14 21:52:09 +0100

    implement methods

--- backend/src/main/java/at/htl/entity/ProjectMember.java
+++ backend/src/main/java/at/htl/entity/ProjectMember.java
@@ -18,4 +18,46 @@ public class ProjectMember {
     @ManyToOne
     @JoinColumn(name = "PM_PERSON")
     private Person person;
+
+    public ProjectMember() {
+    }
+
+    public ProjectMember(Long id, Project project, Person person) {
+        this.id = id;
+        this.project = project;
+        this.person = person;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return "ProjectMember{" +
+                "id=" + id +
+                ", project=" + project +
+                ", person=" + person +
+                '}';
+    }
 }
