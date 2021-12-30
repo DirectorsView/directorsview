7f000ae0a8964e012efc9a4c844fbe31e1a2750b
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-30 12:08:05 +0100

    add fields

--- backend/src/main/java/at/htl/entity/Vacancy.java
+++ backend/src/main/java/at/htl/entity/Vacancy.java
@@ -1,4 +1,18 @@
 package at.htl.entity;
 
+import java.time.LocalDate;
+
 public class Vacancy {
+
+    private Long id;
+
+    private String title;
+
+    private LocalDate deadline;
+
+    private Boolean opened;
+
+    private Company company;
+
+    private Project project;
 }
