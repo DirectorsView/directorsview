aedd905bb068563ba9785ae61efb3bc1f0d7a94c
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-30 12:11:51 +0100

    implement db annotations

--- backend/src/main/java/at/htl/entity/Vacancy.java
+++ backend/src/main/java/at/htl/entity/Vacancy.java
@@ -1,18 +1,33 @@
 package at.htl.entity;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
-public class Vacancy {
+@Entity
+@Table(name = "DV_VAC")
+public class Vacancy extends PanacheEntityBase {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "VAC_ID")
     private Long id;
 
+    @Column(name = "VAC_TITLE")
     private String title;
 
+    @Column(name = "VAC_DEADLINE")
     private LocalDate deadline;
 
+    @Column(name = "VAC_OPENED")
     private Boolean opened;
 
+    @ManyToOne
+    @JoinColumn(name = "VAC_COMPANY")
     private Company company;
 
+    @ManyToOne
+    @JoinColumn(name = "VAC_PROJECT")
     private Project project;
 }
