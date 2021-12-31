47bc3111b96f4a4a25fd24b3b400d1a88c9ad43a
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-31 11:31:38 +0100

    #1 implement getAll Endpoint

--- backend/src/main/java/at/htl/boundary/VacancyService.java
+++ backend/src/main/java/at/htl/boundary/VacancyService.java
@@ -2,11 +2,15 @@ package at.htl.boundary;
 
 import at.htl.control.ApplicantRepository;
 import at.htl.control.VacancyRepository;
+import at.htl.entity.Project;
+import at.htl.entity.Vacancy;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/vacancy")
 @Produces(MediaType.APPLICATION_JSON)
@@ -17,4 +21,9 @@ public class VacancyService {
 
     @Inject
     ApplicantRepository applicantRepository;
+
+    @GET
+    public List<Vacancy> getAll() {
+        return vacancyRepository.listAll();
+    }
 }
