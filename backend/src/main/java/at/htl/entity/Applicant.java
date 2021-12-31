502ed4d98ca128441b9f51d1bec3acf2e697171d
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-31 10:39:43 +0100

    implement annotations

--- backend/src/main/java/at/htl/entity/Applicant.java
+++ backend/src/main/java/at/htl/entity/Applicant.java
@@ -1,8 +1,21 @@
 package at.htl.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "DV_APP")
 public class Applicant {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "APP_ID")
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "APP_VACANCY")
     private Vacancy vacancy;
+
+    @ManyToOne
+    @JoinColumn(name = "APP_PERSON")
     private Person person;
 }
