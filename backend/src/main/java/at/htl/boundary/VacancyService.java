12c16cc8b5a613209b05d5939b15067696430f1e
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-31 11:27:42 +0100

    #1 inject repositories

--- backend/src/main/java/at/htl/boundary/VacancyService.java
+++ backend/src/main/java/at/htl/boundary/VacancyService.java
@@ -1,5 +1,9 @@
 package at.htl.boundary;
 
+import at.htl.control.ApplicantRepository;
+import at.htl.control.VacancyRepository;
+
+import javax.inject.Inject;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -7,4 +11,10 @@ import javax.ws.rs.core.MediaType;
 @Path("/vacancy")
 @Produces(MediaType.APPLICATION_JSON)
 public class VacancyService {
+
+    @Inject
+    VacancyRepository vacancyRepository;
+
+    @Inject
+    ApplicantRepository applicantRepository;
 }
