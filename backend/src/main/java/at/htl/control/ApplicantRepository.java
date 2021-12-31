67cf2cb654e403f935a5711c52972b505d528161
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-31 10:46:18 +0100

    #1 add annotations

--- backend/src/main/java/at/htl/control/ApplicantRepository.java
+++ backend/src/main/java/at/htl/control/ApplicantRepository.java
@@ -1,4 +1,12 @@
 package at.htl.control;
 
-public class ApplicantRepository {
+import at.htl.entity.Applicant;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+@Transactional
+public class ApplicantRepository implements PanacheRepository<Applicant> {
 }
