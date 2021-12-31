59b68871fd064b756b4b71f93068eeadcf583ef5
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-12-31 10:40:32 +0100

    implement constructors

--- backend/src/main/java/at/htl/entity/Applicant.java
+++ backend/src/main/java/at/htl/entity/Applicant.java
@@ -1,10 +1,12 @@
 package at.htl.entity;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "DV_APP")
-public class Applicant {
+public class Applicant extends PanacheEntityBase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,4 +20,14 @@ public class Applicant {
     @ManyToOne
     @JoinColumn(name = "APP_PERSON")
     private Person person;
+
+    public Applicant() {
+    }
+
+    public Applicant(Long id, Vacancy vacancy, Person person) {
+        this.id = id;
+        this.vacancy = vacancy;
+        this.person = person;
+    }
+
 }
