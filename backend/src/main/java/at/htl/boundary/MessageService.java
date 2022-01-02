3f7d61341ce53f641bbea26d6695e34c7a5f0fc2
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2022-01-02 16:03:35 +0100

    #2 add annotations

--- backend/src/main/java/at/htl/boundary/MessageService.java
+++ backend/src/main/java/at/htl/boundary/MessageService.java
@@ -1,4 +1,10 @@
 package at.htl.boundary;
 
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/message")
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageService {
 }
