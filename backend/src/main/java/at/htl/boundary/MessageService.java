b1a0c9fd4aeaa37a3ebd7e864d5d1da323f19590
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2022-01-02 16:04:04 +0100

    #2 inject MessageRepository

--- backend/src/main/java/at/htl/boundary/MessageService.java
+++ backend/src/main/java/at/htl/boundary/MessageService.java
@@ -1,5 +1,8 @@
 package at.htl.boundary;
 
+import at.htl.control.MessageRepository;
+
+import javax.inject.Inject;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -7,4 +10,7 @@ import javax.ws.rs.core.MediaType;
 @Path("/message")
 @Produces(MediaType.APPLICATION_JSON)
 public class MessageService {
+
+    @Inject
+    MessageRepository messageRepository;
 }
