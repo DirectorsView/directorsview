ae6344222750442d0a8c6a4691d26706a78b4375
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2022-01-02 15:54:46 +0100

    #2 add fields

--- backend/src/main/java/at/htl/entity/Message.java
+++ backend/src/main/java/at/htl/entity/Message.java
@@ -1,4 +1,16 @@
 package at.htl.entity;
 
-public class Message {
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
+
+public class Message extends PanacheEntityBase {
+    private Long id;
+    private Account destination;
+    private Account source;
+    private String content;
+    private Timestamp time;
 }
