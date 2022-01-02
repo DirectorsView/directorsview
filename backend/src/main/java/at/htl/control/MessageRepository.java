b3b1e1aa1d67d131de2ee2e9420e91e0d6b01640
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2022-01-02 16:01:37 +0100

    #2 implement save method

--- backend/src/main/java/at/htl/control/MessageRepository.java
+++ backend/src/main/java/at/htl/control/MessageRepository.java
@@ -1,9 +1,17 @@
 package at.htl.control;
 
+import at.htl.entity.Message;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
+
+import static io.quarkus.hibernate.orm.panache.Panache.getEntityManager;
 
 @ApplicationScoped
 @Transactional
 public class MessageRepository {
+
+    public Message save(Message message) {
+        return getEntityManager().merge(message);
+    }
 }
