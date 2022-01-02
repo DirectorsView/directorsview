6ca664fd48af6c1b3e967f205aa6d01d9289b5c0
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2022-01-02 15:59:46 +0100

    #2 implement repository annotations

--- backend/src/main/java/at/htl/control/MessageRepository.java
+++ backend/src/main/java/at/htl/control/MessageRepository.java
@@ -1,4 +1,9 @@
 package at.htl.control;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+@Transactional
 public class MessageRepository {
 }
