a08b18741e6df1ddc511092f3a09413fd550e879
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2022-01-02 16:05:34 +0100

    #2 implement getAll method

--- backend/src/main/java/at/htl/boundary/MessageService.java
+++ backend/src/main/java/at/htl/boundary/MessageService.java
@@ -1,11 +1,15 @@
 package at.htl.boundary;
 
 import at.htl.control.MessageRepository;
+import at.htl.entity.Message;
+import at.htl.entity.Project;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/message")
 @Produces(MediaType.APPLICATION_JSON)
@@ -13,4 +17,9 @@ public class MessageService {
 
     @Inject
     MessageRepository messageRepository;
+
+    @GET
+    public List<Message> getAll() {
+        return messageRepository.listAll();
+    }
 }
