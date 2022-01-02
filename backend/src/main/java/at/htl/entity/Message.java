d613a2a85175a523f926204db025ba6348d51c9d
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2022-01-02 15:57:09 +0100

    #2 add annotations

--- backend/src/main/java/at/htl/entity/Message.java
+++ backend/src/main/java/at/htl/entity/Message.java
@@ -2,15 +2,29 @@ package at.htl.entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.sql.Timestamp;
 
-
+@Entity
+@Table(name = "DV_MES")
 public class Message extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MES_ID")
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MES_DESTINATION")
     private Account destination;
+
+    @ManyToOne
+    @JoinColumn(name = "MES_SOURCE")
     private Account source;
+
+    @Column(name = "MES_CONTENT")
     private String content;
+
+    @Column(name = "MES_TIME")
     private Timestamp time;
 }
