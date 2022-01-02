3ebfd3695047bafc8d19620396071ac447b65573
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2022-01-02 16:05:07 +0100

    #2 implement PanacheRepository

--- backend/src/main/java/at/htl/control/MessageRepository.java
+++ backend/src/main/java/at/htl/control/MessageRepository.java
@@ -1,6 +1,8 @@
 package at.htl.control;
 
 import at.htl.entity.Message;
+import at.htl.entity.Project;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
@@ -9,7 +11,7 @@ import static io.quarkus.hibernate.orm.panache.Panache.getEntityManager;
 
 @ApplicationScoped
 @Transactional
-public class MessageRepository {
+public class MessageRepository implements PanacheRepository<Message> {
 
     public Message save(Message message) {
         return getEntityManager().merge(message);
