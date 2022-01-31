8b1855b94053ac217a9fd8184eb9e6700916a387
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2022-01-31 11:27:54 +0100

    add new endpoints

--- backend/src/main/java/at/htl/boundary/PersonService.java
+++ backend/src/main/java/at/htl/boundary/PersonService.java
@@ -1,10 +1,11 @@
 package at.htl.boundary;
 
 import at.htl.control.PersonRepository;
-import at.htl.entity.Company;
 import at.htl.entity.Person;
+import at.htl.entity.Project;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -31,4 +32,39 @@ public class PersonService {
     public Person getOne(@PathParam("id") Long id) {
         return repository.findById(id);
     }
+
+    @GET
+    @Path("/{id}/projects")
+    public List<Project> getProjects(@PathParam("id") Long id) {
+        return repository.findProjects(id);
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Person put(@PathParam("id") long id, Person person) {
+        Person originalPerson = repository.findById(id);
+
+        if (originalPerson != null) {
+            originalPerson.setPassword(person.getPassword());
+            originalPerson.setEmail(person.getEmail());
+            originalPerson.setWebsite(person.getWebsite());
+            originalPerson.setFirstName(person.getFirstName());
+            originalPerson.setLastName(person.getLastName());
+            originalPerson.setBirthdate(person.getBirthdate());
+        }
+
+        return originalPerson;
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Person delete(@PathParam("id") long id) {
+        Person person = repository.findById(id);
+        repository.delete("id = " + id);
+        return person;
+    }
+
 }
