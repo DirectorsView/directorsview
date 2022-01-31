ad9aa990409676eac26f96b49ca291daf62eb6b8
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2022-01-31 11:25:59 +0100

    add method findProjects

--- backend/src/main/java/at/htl/control/PersonRepository.java
+++ backend/src/main/java/at/htl/control/PersonRepository.java
@@ -1,15 +1,26 @@
 package at.htl.control;
 
 import at.htl.entity.Person;
+import at.htl.entity.Project;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @ApplicationScoped
 @Transactional
 public class PersonRepository implements PanacheRepository<Person> {
+
     public Person save(Person person) {
         return getEntityManager().merge(person);
     }
+
+    public List<Project> findProjects(Long personId) {
+        return getEntityManager()
+                .createNamedQuery("Person.findProjects", Project.class)
+                .setParameter("personId", personId)
+                .getResultList();
+    }
+
 }
