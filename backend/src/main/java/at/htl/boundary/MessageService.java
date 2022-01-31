05797e6145d485a8a61a1b20c0020f884a485ac9
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2022-01-31 11:27:33 +0100

    add update and delete endpoints

--- backend/src/main/java/at/htl/boundary/MessageService.java
+++ backend/src/main/java/at/htl/boundary/MessageService.java
@@ -2,9 +2,9 @@ package at.htl.boundary;
 
 import at.htl.control.MessageRepository;
 import at.htl.entity.Message;
-import at.htl.entity.Project;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -32,4 +32,31 @@ public class MessageService {
     public Message getOne(@PathParam("id") Long id) {
         return messageRepository.findById(id);
     }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Message put(@PathParam("id") long id, Message message) {
+        Message originalMessage = messageRepository.findById(id);
+
+        if (originalMessage != null) {
+            originalMessage.setDestination(message.getDestination());
+            originalMessage.setSource(message.getSource());
+            originalMessage.setContent(message.getContent());
+            originalMessage.setTime(message.getTime());
+        }
+
+        return originalMessage;
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Message delete(@PathParam("id") long id) {
+        Message message = messageRepository.findById(id);
+        message.delete("id = " + id);
+        return message;
+    }
+
 }
